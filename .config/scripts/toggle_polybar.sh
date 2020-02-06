db0452ecb9f5a097308ee39453fafe413d2c7f62
Danang Rahmatullah <dasyad00@gmail.com>
2020-02-06 22:32:49 +0100

    Added toggle_polybar

--- .config/scripts/toggle_polybar.sh
+++ .config/scripts/toggle_polybar.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
+bar_size=42
+current_padding=$(bspc config -m focused top_padding)
+new_padding=0
+if [ "${current_padding}" == "${bar_size}"  ]; then
+    # Remove padding
+    new_padding=0
+else
+    # Add padding
+    new_padding=${bar_size}
+fi
+polybar-msg cmd toggle
 
+echo ${new_padding}
+bspc config -m focused top_padding ${new_padding}
